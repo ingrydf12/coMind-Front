b57354d42a8cdc14988b33b1c3c089a52a574e0f
Ingryd <ingrydcduarte@gmail.com>
2025-01-26 19:15:47 -0300

    feat: router basic

---
+++ src/components/RedirectRoute/RedirectRoute.jsx
@@ -0,0 +1,14 @@
+const RedirectRoute = ({ children }) => {
+    // Criar um context do token para fazer o redirecionamento
+    // Exemplo: 
+    // const {token} = useToken()
+
+    // if(token){
+    //     return <Navigate to="/" replace />
+    // }
+    // return children
+
+    return children
+}
+
+export default RedirectRoute
