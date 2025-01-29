80812353533b7ac6ba26f33e2b6d3f7927282d54
Ingryd <ingrydcduarte@gmail.com>
2025-01-29 19:34:52 -0300

    fix: navigate banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,19 @@
 import '../../styles/Banner.css';
+import { useNavigate } from 'react-router-dom';
 
 const Banner = () => { 
+    const navigate = useNavigate();
+    const handleRedirect = () => {
+        navigate('/login');  //descobri que devo usar a / pra indicar que é um caminho absoluto
+      };
+      
     return (
         <div>
            <div className="banner">
                 <div className="banner-text">
                     <h1>VOCÊ NÃO ESTÁ SOZINHO!</h1>
                     <p>Nós da coMind somos uma organização não governamental dedicada a oferecer apoio psicológico gratuito. Conectamos profissionais qualificados a indivíduos que necessitam de ajuda. <strong>Vamos nessa?</strong></p>
-                <button className="btnAjuda">Conecte-se agora</button></div>
+                <button className="btnAjuda" onClick={handleRedirect}>Conecte-se agora</button></div>
                 <img src="src/assets/homeHeart.svg" alt="homeHeart"></img>
 
            </div>
