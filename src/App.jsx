cdadccab94be8cbe7f6383da868dd81d2234e338
Ingryd <ingrydcduarte@gmail.com>
2025-01-29 10:45:20 -0300

    feat: mock dados

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import RedirectRoute from './components/RedirectRoute/RedirectRoute'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
 import Layout from './components/Layout/Layout'
+import Dashboard from './pages/Dashboard/Dashboard'
 
 export const router = createBrowserRouter([
   {
@@ -11,7 +12,15 @@ export const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        path: "/auth",
+        path: "auth",
+        element: (
+          <RedirectRoute>
+            <Connect />
+          </RedirectRoute>
+        )
+      },
+      {
+        path: "login",
         element: (
           <RedirectRoute>
             <Login />
@@ -19,10 +28,17 @@ export const router = createBrowserRouter([
         )
       },
       {
-        path: "/register",
+        path: "register",
         element: (
           <RedirectRoute>
             <Register />
+          </RedirectRoute>
+        )
+      },{
+        path: "dashboard",
+        element: (
+          <RedirectRoute>
+            <Dashboard />
           </RedirectRoute>
         )
       }
