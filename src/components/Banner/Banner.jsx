9afba2e83b97fe5043b04ce3e897f0a9c9697958
Ingryd <90623907+ingrydf12@users.noreply.github.com>
2025-01-29 16:25:29 -0300

    add: component Banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,18 @@
+import '../../styles/Banner.css';
+
+const Banner = () => { 
+    return (
+        <div>
+           <div className="banner">
+                <div className="banner-text">
+                    <h1>VOCÊ NÃO ESTÁ SOZINHO!</h1>
+                    <p>Nós da coMind somos uma organização não governamental dedicada a oferecer apoio psicológico gratuito. Conectamos profissionais qualificados a indivíduos que necessitam de ajuda. <strong>Vamos nessa?</strong></p>
+                <button className="btnAjuda">Conecte-se agora</button></div>
+                <img src="src/assets/homeHeart.svg" alt="homeHeart"></img>
+
+           </div>
+        </div>
+    );
+}
+
+export default Banner;
