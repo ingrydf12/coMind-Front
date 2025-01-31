6b935487e37a1307d5688a7625d308fcbadcbd9b
Ingryd <ingrydcduarte@gmail.com>
2025-01-31 08:51:27 -0300

    fix: padrao de cards

--- src/components/Indicacoes/Indicacoes.jsx
+++ src/components/Indicacoes/Indicacoes.jsx
@@ -11,16 +11,15 @@ const Indicacoes = () => {
                     <p>Grupo de apoio virtual focado em lidar com ansiedade e estresse por meio de encontros semanais online. </p>
                 </div>
                 <div className="card2">
-                    <h4>ABRAÇO COLETIVO</h4>
-                    <h5>Presencial</h5>
+                    <h2>ABRAÇO COLETIVO</h2>
+                    <h3>Presencial</h3>
                     <p>Encontros presenciais para apoio psicológico e fortalecimento emocional, realizados em centros comunitários. </p>
                 </div>
                 <div className="card3">
-                    <h6>CONEXÃO VIRTUAL</h6>
-                    <h7>Remoto</h7>
+                    <h2>CONEXÃO VIRTUAL</h2>
+                    <h3>Remoto</h3>
                     <p>Sessões online voltadas para jovens em busca de suporte emocional e autoconhecimento. </p>
                 </div>
-
             </div>
         </div>
     );
