156ee7a1a5bbc999945dacc8cb6c7e32fca3c873
Ingryd <ingrydcduarte@gmail.com>
2025-03-23 14:34:01 -0300

    feat: api (axios)

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL, 
+  baseURL: "http://localhost:3000",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
 
 export default api;
