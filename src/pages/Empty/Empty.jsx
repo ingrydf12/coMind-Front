5f3263b58f5e55b7e92d5c1fc62a9cb34c582505
Ingryd <ingrydcduarte@gmail.com>
2025-03-23 21:40:54 -0300

    feat: empty view

---
+++ src/pages/Empty/Empty.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Empty.css";
+
+const Empty = () => {
+  return (
+  <div className="empty-view">
+    <img src="https://img.freepik.com/free-vector/no-data-concept-illustration_114360-536.jpg"></img>
+    <h1>Sem informação por aqui.</h1>
+    </div>
+  )
+};
+
+export default Empty;
