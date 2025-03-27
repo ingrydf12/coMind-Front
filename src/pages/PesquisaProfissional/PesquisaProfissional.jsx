6bd437feec10f02ede87076264de86e640de9e63
Ingryd <ingrydcduarte@gmail.com>
2025-03-27 11:50:52 -0300

    wip: search page

---
+++ src/pages/PesquisaProfissional/PesquisaProfissional.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PesquisaProfissional = () => {
+    return (
+        <div>
+            <h1>PesquisaProfissional</h1>
+        </div>
+    );
+};
+
+export default PesquisaProfissional;
