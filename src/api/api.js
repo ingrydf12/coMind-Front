8ba0e6382100cc27a147328da48517443095efc8
Ingryd <ingrydcduarte@gmail.com>
2025-03-27 17:33:39 -0300

    feat: auth bearer to api axios

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
+    'Authorization': `Bearer ${sessionStorage.getItem('token')}`
   },
 });
 
