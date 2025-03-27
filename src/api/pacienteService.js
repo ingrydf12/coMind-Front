fd334ff9ea014f59902116d5096e014af4a7270c
Ingryd <ingrydcduarte@gmail.com>
2025-03-27 17:57:22 -0300

    fix: better pacienteService

--- src/api/pacienteService.js
+++ src/api/pacienteService.js
@@ -3,9 +3,3 @@ import api from "./api";
 export const completarPaciente = (id, data) => {
     return api.put(`/paciente/${id}`, data);
 };
-
-const getUserProfile = async () => {
-    return api.get("/paciente");
-};
-
-export default { getUserProfile };
