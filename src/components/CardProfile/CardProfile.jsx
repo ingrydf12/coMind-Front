666e42c31f92fea8bd744530a12dc461a1495733
Ingryd <ingrydcduarte@gmail.com>
2025-03-28 00:17:33 -0300

    wip: card profile component

---
+++ src/components/CardProfile/CardProfile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './CardProfile.css'
+
+const CardProfile = ({ profile }) => {
+    return (
+        <div className="profile-card-container">
+            <p>Teste</p>
+        </div>
+    );
+};
+
+export default CardProfile;
--- src/pages/PesquisaProfissional/PesquisaProfissional.jsx
+++ src/pages/PesquisaProfissional/PesquisaProfissional.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import CardProfile from "../../components/CardProfile/CardProfile";
 
 const PesquisaProfissional = () => {
     return (
         <div>
             <h1>PesquisaProfissional</h1>
+            <CardProfile />
         </div>
     );
 };
