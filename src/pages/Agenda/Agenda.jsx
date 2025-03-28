278717ebbc8cf932e21f81776b71e94fa2676853
marxvictor <59874241+marxvictor@users.noreply.github.com>
2025-03-27 22:28:41 -0300

    Update Agenda.jsx

--- src/pages/Agenda/Agenda.jsx
+++ src/pages/Agenda/Agenda.jsx
@@ -1,11 +1,42 @@
-import './Agenda.css';
+import React from "react";
 
 const Agenda = () => {
     return(
+        <>
         <div>
-            <h1>Testando!</h1>
+            <label htmlFor="especialidade">Especialidade</label>
+            <select name="" id="especialidade">
+                <option>Depressão</option>
+                <option>Ansiedade</option>
+                <option>Transtorno de estresse pós-traumático</option>
+                <option>Esquizofrenia</option>
+                <option>Transtorno bipolar</option>
+                <option>Transtornos de personalidade</option>
+                <option>Transtornos alimentares</option>
+                <option>Transtorno do pânico</option>
+                <option>Transtorno de déficit de atenção com hiperatividade (TDAH)</option>
+                <option>Transtornos de estresse</option>
+            </select>
+            <label for="localização">Localização</label> 
+            <input type="text" name="localização" id="" />
+            <label for="convenio">Convênio</label>
+            <select name="" id="convenio">
+                <option value="">Bradesco</option>
+                <option value="">Caixa</option>
+                <option value="">Correios</option>
+                <option value="">Hapvida</option>
+                <option value="">Itaú</option>
+                <option value="">Petrobrás</option>
+                <option value="">Unimed</option>
+            </select>
+            <input type="radio" name="type" id="p" />
+            <label for="p">Presencial</label>
+            <input type="radio" name="type" id="r" />
+            <label for="r">Remoto</label>
         </div>
-    )
-}
+        <div>
 
-export default Agenda;
+        </div>
+        </>
+    );
+}
